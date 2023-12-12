8fc3d2e6edfb3dd6130a1ebe80ac401e45d9988b
Rinad Sohail <rinads124@outlook.com>
2023-12-12 13:20:59 +0000

    made changes to date posted in home.html

--- vegify/main/views.py
+++ vegify/main/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'RinadS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'December 05, 2023'
-    },
-    {
-        'author': 'AdamS',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'December 06, 2023'
-    }
-]
 
 
 def home(request):
@@ -22,6 +8,7 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'main/home.html', context)
+
 
 def about(request):
     return render(request, 'main/about.html', {'title': 'About'})
