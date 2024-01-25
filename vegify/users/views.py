6de9ddede06fe953ea160b8aa9fd097767dc7961
Rinad Sohail <rinads124@outlook.com>
2024-01-25 11:08:38 +0000

    edits to files

--- vegify/users/views.py
+++ vegify/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
-
+# create a register view - uses request, passed to template, using a premade function that allows for the creation of a user
 def register(request):
     form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
