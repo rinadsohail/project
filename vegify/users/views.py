bb919f99c93e63abfc478cbcbd02c3cad7e67c74
Rinad Sohail <rinads124@outlook.com>
2024-01-30 09:33:12 +0000

    made username check & account creation

--- vegify/users/views.py
+++ vegify/users/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # create a register view - uses request, passed to template, using a premade function that allows for the creation of a user
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST': 
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('main-home')
+    else: 
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
