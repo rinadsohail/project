506422adc20bbc0d1eefdb68d83b3d0e09e9d014
Rinad Sohail <rinads124@outlook.com>
2024-02-08 11:07:44 +0000

    abstract user types

--- vegify/account/urls.py
+++ vegify/account/urls.py
@@ -1,1 +1,3 @@
 from django.urls import include, path
+from .views import 
+
--- vegify/users/models.py
+++ vegify/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from django.contrib.auth.models import AbstractUser
 
 
 class Profile(models.Model):
@@ -8,4 +9,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        CUSTOMER = 'CUSTOMER', 'Customer'
+        BUSINESS = 'BUSINESS', 'Business'
+
+    base_role = Role.ADMIN 
+    
+
+
     
